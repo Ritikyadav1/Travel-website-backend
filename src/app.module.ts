4f7518df90aca3dd9815e514c5cf402d647a6d16
Ritik Yadav <ritikyadav10032004@gmail.com>
2025-10-03 00:39:45 +0530

    import auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -23,6 +24,7 @@ import { ConfigModule } from '@nestjs/config';
       autoLoadEntities: true,
     }),
     UserModule,
+    AuthModule,
   ],
 
   controllers: [AppController],
